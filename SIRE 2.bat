96522ce3dc5194987b684aca6c09f5c1050054c2
Agustin Bustos Piasentini <bustos-agustin@hotmail.com - agustin.bustos.p@gmail.com>
2023-06-09 14:19:14 -0300

    Update .Bat

--- SIRE 2.bat
+++ SIRE 2.bat
@@ -1,3 +1,3 @@
 @echo off
-"F:\Proyectos Python\BIN_EV\Scripts\python.exe" "F:\Proyectos Python\SIRE\Arreglar SIRE Con interfaz.py"
+"F:\Proyectos Python\BIN_EV\Scripts\python.exe" "F:\Proyectos Python\Scripts\SIRE\Arreglar SIRE Con interfaz.py"
 pause
